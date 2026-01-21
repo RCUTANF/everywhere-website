f2318267c9809383e5416c6d5e0d3fa1611e67e8
Artemis Li <auroraziling@gmail.com>
2026-01-22 00:45:05 +0800

    use user language

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import { i18n } from '@/lib/i18n';
 import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
 
-export default function RootPage() {
+export default async function RootPage() {
+  const headersList = await headers();
+  const acceptLanguage = headersList.get('accept-language');
+
+  if (acceptLanguage?.includes('zh')) {
+    redirect('/zh-CN');
+  }
+
   redirect(`/${i18n.defaultLanguage}`);
 }
