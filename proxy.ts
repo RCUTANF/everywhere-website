06909cd6fb5a8e13329cbb0a65acd036e2bd3930
Artemis Li <auroraziling@gmail.com>
2026-01-22 18:50:56 +0800

    update compatibility date

--- proxy.ts
+++ proxy.ts
@@ -4,8 +4,6 @@ import { i18n } from '@/lib/i18n';
 export default createI18nMiddleware(i18n);
 
 export const config = {
-  runtime: 'edge', 
-
   matcher: [
     '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico|txt|xml|json|woff|woff2|ttf|eot)).*)',
   ],
